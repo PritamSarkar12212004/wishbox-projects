8099d89852b8799622a1642597d091e23efd0931
PritamSarkar12212004 <162453679+PritamSarkar12212004@users.noreply.github.com>
2024-05-16 14:05:41 +0530

    solve database

--- index.js
+++ index.js
@@ -2,21 +2,51 @@
 const express = require("express");
 const ejsMate = require("ejs-mate");
 const path = require("path");
+const mongoose = require("mongoose");
 require("dotenv").config();
 //! require mongoose
-const mongooseServer = require("./server/mongoose"); // Corrected the import statement
+
 //! require routes
-const indexRoutes = require("./routes/index");
+const indexRoutes = require("./routes/index.js");
+const adminRoutes = require("./routes/admi.js");
 
 //* express server
 const app = express();
 const port = process.env.PORT;
-mongooseServer(); // Start mongoose server
-
 app.listen(port, () => {
   console.log("Server started at port " + port);
 });
+//* mongo srver
+async function mongooseServer() {
+  const maxRetries = 5; // Maximum number of connection retry attempts
+  const retryDelay = 3000; // Delay between retry attempts in milliseconds (3 seconds in this example)
+  let retries = 0;
 
+  while (retries < maxRetries) {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI);
+      console.log("Database is running");
+      return; // If the connection is successful, exit the function
+    } catch (error) {
+      console.error(
+        `Database connection error (Attempt ${retries + 1}):`,
+        error
+      );
+      retries++;
+      // If maximum retries reached, log the failure and exit the function
+      if (retries === maxRetries) {
+        console.error(
+          "Maximum retry attempts reached. Unable to connect to the database."
+        );
+        process.exit(1);
+      }
+      console.log(`Retrying connection in ${retryDelay / 1000} seconds...`);
+      await new Promise((resolve) => setTimeout(resolve, retryDelay)); // Wait before retrying
+    }
+  }
+}
+
+mongooseServer();
 //* set use and static
 app.engine("ejs", ejsMate);
 app.set("views", path.join(__dirname, "views"));
@@ -25,6 +55,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 //? routes
 app.use("/", indexRoutes);
+app.use("/admin", adminRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
