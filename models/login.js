e1f492998aae04ba5d89f605f31ca441c3183e80
PritamSarkar12212004 <162453679+PritamSarkar12212004@users.noreply.github.com>
2024-05-17 23:08:39 +0530

    today work is complate

--- models/login.js
+++ models/login.js
@@ -1,1 +1,18 @@
 const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    },
+    PhoneNo: {
+        type: Number,
+        required:true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+const user = mongoose.model("user", userSchema);
+
+module.exports = user;
--- routes/admi.js
+++ routes/admi.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cards = require("../models/cards");
 const poster = require("../models/posters");
 const Adver = require("../models/advertisement");
+const user = require("../models/login");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   let postar = await poster.find();
   let CardData = await cards.find();
   let AdverData = await Adver.find();
-  res.render("layouts/Admain.ejs", { CardData, postar, AdverData });
+  let userData = await user.find();
+  res.render("layouts/Admain.ejs", { CardData, postar, AdverData,userData });
 });
 module.exports = router;
